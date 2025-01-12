aca7adef327d12173630b1dc6500f0803b99601f
kkavyaGoyal2003 <kkavyagoyal2008@gmail.com>
2025-01-12 10:24:13 +0530

    new commit

--- WebPrac/prac1.js
+++ WebPrac/prac1.js
@@ -1,6 +1,14 @@
+let start = Date.now();
 function Sum(n) {
     var sum = 0;
-    for(let i = 0; i <= n; i++) {
-        
+    for(let i = 1; i <= n; i++) {
+        sum = sum + i;
     }
+    return sum;
 }
+
+var ans1 = Sum(1000000);
+console.log(ans1);
+let end = Date.now();
+
+console.log ((end - start) / 1000);
