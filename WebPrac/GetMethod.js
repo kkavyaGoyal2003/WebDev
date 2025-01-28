9cf29807278e57753a44448530eff4434af0ad80
kkavyaGoyal2003 <kkavyagoyal2008@gmail.com>
2025-01-28 08:54:30 +0530

    Post method

--- WebPrac/GetMethod.js
+++ WebPrac/GetMethod.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const port = 3000
 
+// function middleware(req, res, next) {
+//     console.log("Inside middleware!!" + req.headers.counter);
+//     next();
+// }
+
+// app.use(middleware);
 function Message(str) {
     return str;
 }
@@ -11,7 +17,8 @@ function handleFirstRequest(req, res) {
     res.send(ans);
 }
 
-app.get('/message', handleFirstRequest);
+// app.get('/message', handleFirstRequest);
+app.post('/message', handleFirstRequest);
 
 function started() {
     console.log(`Example app listening on port ${port}`);
